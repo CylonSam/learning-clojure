42171c80a8f40c7de2096e9953b5b6a72b53ad1d
Samantha Silva <samanthaathayde@hotmail.com>
2023-09-19 21:53:30 -0300

    initial commit

---
+++ playground.clj
@@ -0,0 +1,25 @@
+; basic math
+(+ 17 25)
+(* 6 7)
+(- 45 3)
+(/ 84 2)
+
+; defining a variable
+(def programming-language "Clojure")
+
+(println programming-language "is definitely something!")
+
+; defining a function and calling it
+(defn welcome-message
+  []
+  (println "Welcome to Clojure Hell!"))
+
+(welcome-message)
+
+; using let
+(defn welcome-message-with-name
+  [name]
+  (let [message (str "Welcome to Clojure Hell, " name "!")]
+    (println message)))
+
+(welcome-message-with-name "Sam")
